52893041e85a523bd6bbe55b6aae65ae6e62670c
Ximing Cheng <chengximing1989@gmail.com>
2014-07-15 19:48:30 +0800

    Change settings of server

--- iSport_server/iSport_server/settings.py
+++ iSport_server/iSport_server/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'iSport_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'iSport'),
+		'USER': 'root',
+		'PASSWORD': '123',
+		'HOST': 'efly.freeshell.ustc.edu.cn',
+		'PORT': '54323'
     }
 }
 

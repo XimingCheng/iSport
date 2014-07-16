0d4afe60e1d51181e6381123f593281f5857fe49
XimingCheng <chengximing1989@gmail.com>
2014-07-16 21:43:22 +0800

    Fix the server mistakes of reg

--- iSport_server/iSport_server/iSport/models.py
+++ iSport_server/iSport_server/iSport/models.py
@@ -4,14 +4,14 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class User(models.Model):
     name = models.CharField(max_length = 100)
     password = models.CharField(max_length = 200)
-    location = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100, blank = True)
     score = models.FloatField(validators = [MinValueValidator(0.0),
-        MaxValueValidator(5.0)])
+        MaxValueValidator(5.0)],  default = 5.0)
     completed_id = models.CharField(max_length = 1000, blank = True)
     uncompleted_id = models.CharField(max_length = 1000, blank = True)
     sex = models.CharField(max_length = 2, default = 'F')
-    user_image = models.CharField(max_length = 1024)
-    label = models.CharField(max_length = 1000)
+    user_image = models.CharField(max_length = 1024, default = 'test.png')
+    label = models.CharField(max_length = 1000, blank = True)
 
 
 class Activity(models.Model):

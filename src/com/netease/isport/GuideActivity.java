3927164afda7a11971acdc972ba5499f5c20f9d0
Ximing Cheng <chengximing1989@gmail.com>
2014-07-17 19:19:24 +0800

    Fix the guide three page btn

--- src/com/netease/isport/GuideActivity.java
+++ src/com/netease/isport/GuideActivity.java
@@ -2,7 +2,6 @@ package com.netease.isport;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -18,10 +17,8 @@ public class GuideActivity extends Activity implements OnPageChangeListener {
 	private ViewPagerAdapter vpAdapter;
 	private List<View> views;
 
-	// 底部小点图片
 	private ImageView[] dots;
 
-	// 记录当前选中位置
 	private int currentIndex;
 
 	@Override
@@ -29,10 +26,8 @@ public class GuideActivity extends Activity implements OnPageChangeListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.guide);
 
-		// 初始化页面
 		initViews();
 
-		// 初始化底部小点
 		initDots();
 	}
 
@@ -40,18 +35,14 @@ public class GuideActivity extends Activity implements OnPageChangeListener {
 		LayoutInflater inflater = LayoutInflater.from(this);
 
 		views = new ArrayList<View>();
-		// 初始化引导图片列表
 		views.add(inflater.inflate(R.layout.what_new_one, null));
 		views.add(inflater.inflate(R.layout.what_new_two, null));
 		views.add(inflater.inflate(R.layout.what_new_three, null));
 
-
-		// 初始化Adapter
 		vpAdapter = new ViewPagerAdapter(views, this);
 		
 		vp = (ViewPager) findViewById(R.id.viewpager);
 		vp.setAdapter(vpAdapter);
-		// 绑定回调
 		vp.setOnPageChangeListener(this);
 	}
 
@@ -60,14 +51,13 @@ public class GuideActivity extends Activity implements OnPageChangeListener {
 
 		dots = new ImageView[views.size()];
 
-		// 循环取得小点图片
 		for (int i = 0; i < views.size(); i++) {
 			dots[i] = (ImageView) ll.getChildAt(i);
-			dots[i].setEnabled(true);// 都设为灰色
+			dots[i].setEnabled(true);
 		}
 
 		currentIndex = 0;
-		dots[currentIndex].setEnabled(false);// 设置为白色，即选中状态
+		dots[currentIndex].setEnabled(false);
 	}
 
 	private void setCurrentDot(int position) {
@@ -82,21 +72,17 @@ public class GuideActivity extends Activity implements OnPageChangeListener {
 		currentIndex = position;
 	}
 
-	// 当滑动状态改变时调用
 	@Override
 	public void onPageScrollStateChanged(int arg0) {
 	}
 
-	// 当当前页面被滑动时调用
 	@Override
 	public void onPageScrolled(int arg0, float arg1, int arg2) {
 	}
 
-	// 当新的页面被选中时调用
 	@Override
 	public void onPageSelected(int arg0) {
-		// 设置底部小点选中状态
-		setCurrentDot(arg0);
+		
 	}
 
 }
